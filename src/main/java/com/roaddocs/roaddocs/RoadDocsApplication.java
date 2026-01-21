08fb69f475ccdcc4384a9b980376e15c9a4be8f5
Otavio-Emanoel <otavioemanuel6b@gmail.com>
2026-01-20 21:26:08 -0300

    Initial config of the Spring Boot and dependencies

---
+++ src/main/java/com/roaddocs/roaddocs/RoadDocsApplication.java
@@ -0,0 +1,13 @@
+package com.roaddocs.roaddocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadDocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadDocsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roaddocs/roaddocs/RoadDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roaddocs.roaddocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadDocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
